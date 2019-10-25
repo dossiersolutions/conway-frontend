eba2bd7424e59f08f5230cb4520d931b29027376
jovan <jovan.milutinovic84@gmail.com>
2019-10-25 13:20:25 +0200

    Connecting components

--- src/GameScreen.js
+++ src/GameScreen.js
@@ -1,20 +1,20 @@
 import React, {Component} from "react";
+import {GameCanvas} from "./GameCanvas";
+import {GAME_STATE_BASIC, GameState} from "./GameState/GameState";
 
 class GameScreen extends Component {
+  constructor(props) {
+    super(props);
 
-  makeList() {
-    return (
-        <div>
-          <p>sadfsdf</p>
-          <p>asdadasdasd</p>
-        </div>
-    )
+    this.state = {
+      canvasDefaultState: new GameState(GAME_STATE_BASIC, 1, 1, [1])
+    }
   }
 
   render() {
     return (
         <div className="MainScreen" style={{marginBottom: "20px", marginTop: "10px"}}>
-
+          <GameCanvas gameState={this.state.canvasDefaultState}/>
         </div>
     );
   }
